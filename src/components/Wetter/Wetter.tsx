9b45c42f97166c96d760f90c71c5ddb7ebf5b40c
The Loraxxz <tilmann.lorenz@mail.com>
2021-10-12 09:26:45 +0200

    Test

---
+++ src/components/Wetter/Wetter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Wetter.module.scss';
+
+class Wetter extends React.Component {
+  render() {
+    return <div className={styles.Wetter}>
+              Test
+          </div>;
+  }
+  getBackendAPI(){
+    
+  }
+}
+
+export default Wetter;
