d3be7593c587faa04607fb652bbceeda2087c0cf
The Loraxxz <tilmann.lorenz@mail.com>
2021-10-17 19:03:49 +0200

    Wetter test

--- src/components/Wetter/Wetter.tsx
+++ src/components/Wetter/Wetter.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './Wetter.module.scss';
+import axios from 'axios';
 
+const ApiKey = "d6971f3426dc09726ea595de7cf0dff6";
 class Wetter extends React.Component {
   render() {
     return <div className={styles.Wetter}>
@@ -8,7 +10,7 @@ class Wetter extends React.Component {
           </div>;
   }
   getBackendAPI(){
-    
+    axios.get("") 
   }
 }
 
