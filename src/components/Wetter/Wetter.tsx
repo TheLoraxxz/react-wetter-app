de69477be96ed7c18fa43d1e0ae1ca0a7e2cd967
The Loraxxz <tilmann.lorenz@mail.com>
2021-11-02 10:46:21 +0100

    umarbeiten und einfügen der city wheather rstuff

--- src/components/Wetter/Wetter.tsx
+++ src/components/Wetter/Wetter.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import styles from './Wetter.module.scss';
 import axios from 'axios';
+import { weather } from '../../interface/weather';
 
 const ApiKey = "d6971f3426dc09726ea595de7cf0dff6";
 class Wetter extends React.Component {
+  cityWheather: weather|null = null;
   render() {
+    this.getBackendAPI("Dresden");
     return <div className={styles.Wetter}>
-              Test
+              {this.cityWheather==null ? "":this.cityWheather.description} <button></button>
           </div>;
   }
-  getBackendAPI(){
-    axios.get("") 
+  getBackendAPI(city:string){
+    axios.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ApiKey).then((res)=>{
+      this.cityWheather = {
+        description: res.data["weather"][0]["description"],
+        icon: res.data["weather"][0]["icon"],
+        temperatur: res.data["main"]["temp"]-273.15,
+        tempMin: res.data["main"]["temp_min"]-273.15,
+        tempMax: res.data["main"]["temp_max"]-273.15,
+        name: city,
+        humidity:res.data["main"]["humidity"],
+        sunrise: new Date(res.data["sys"]["sunrise"]*1000),
+        sunset: new Date(res.data["sys"]["sunset"]*1000),
+      }  
+    })
   }
 }
-
 export default Wetter;
---
+++ src/interface/weather.ts
@@ -0,0 +1,11 @@
+export interface weather {
+    description: string;
+    icon: string;
+    temperatur: number;
+    tempMin:number;
+    tempMax:number;
+    name: string;
+    humidity:number;
+    sunrise: Date;
+    sunset: Date;
+}
