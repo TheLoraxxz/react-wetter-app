e377d19008d5742ae886fc19644b00e67ad3fb91
The Loraxxz <tilmann.lorenz@mail.com>
2022-01-05 10:02:25 +0100

    Added header and started search button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import Wetter from './components/Wetter/Wetter';
+import AppBar from '@mui/material/AppBar';
+import { Box } from '@mui/system';
+import { FormControl, IconButton, InputBase, InputBaseClassKey, Toolbar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
-var test = "Hello World!";
-
+var city:string = 'Berlin';
 function App() {
   return (
-    <div className="App">
-      {test}
-      <Wetter city="Dresden" />
-    </div>
+    <>
+      <Box>
+        <AppBar position="static">
+          <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+            </IconButton>
+            <div className='searchField'>
+              <div className='searchIcon'>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    sx={{ mr: 2 }}>
+                  <SearchIcon />
+                </IconButton>  
+              </div>
+              <InputBase
+                placeholder="Search City"
+                onKeyDown={keyInput}
+                
+              />
+          </div>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div className="App">
+        <Wetter city={city} />
+      </div>
+    </>
   );
+}
+function keyInput(event:React.KeyboardEvent<HTMLInputElement>) {
+  if(event.key=='Enter') {
+    console.log(event.target)
+  }
 }
 
 export default App;
--- src/components/Wetter/Wetter.tsx
+++ src/components/Wetter/Wetter.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import styles from './Wetter.module.scss';
+import styles from './Wetter.module.scss'
 import axios from 'axios';
 import { weather } from '../../interface/weather';
-
+import Card from '@mui/material/Card';
+import { Avatar, CardContent, CardHeader } from '@mui/material';
 const ApiKey = "d6971f3426dc09726ea595de7cf0dff6";
 
 interface IProps {
@@ -27,26 +28,36 @@ class Wetter extends React.Component<IProps, IState> {
   }
 
   render() {
-    return <div className={styles.Wetter}>
-              {this.state.weather?.description} <button></button>
-          </div>;
+    return <Card className={styles.weatherCard}>
+              <CardHeader 
+                avatar = {
+                  <Avatar src={this.state.weather?.icon} alt={this.state.weather?.description}></Avatar>
+                }
+                title={this.state.weather?.name}></CardHeader>
+              <CardContent>
+                <p className={styles.head}>{this.state.weather?.description}</p>
+                <p>Temperatur: {this.state.weather?.temperatur.toFixed(2).replace('.',',')}°C</p>
+                <p></p>
+                
+              </CardContent>
+          </Card>;
   }
   getBackendAPI(city:string){
-    axios.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ApiKey).then((res)=>{
+    axios.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ApiKey+"&lang=de").then((res)=>{
       this.setState({
         weather: {
           description: res.data["weather"][0]["description"],
-          icon: res.data["weather"][0]["icon"],
+          icon: 'http://openweathermap.org/img/wn/'+res.data["weather"][0]["icon"]+".png", 
           temperatur: res.data["main"]["temp"]-273.15,
           tempMin: res.data["main"]["temp_min"]-273.15,
           tempMax: res.data["main"]["temp_max"]-273.15,
-          name: city,
+          name: res.data["name"],
           humidity:res.data["main"]["humidity"],
           sunrise: new Date(res.data["sys"]["sunrise"]*1000),
           sunset: new Date(res.data["sys"]["sunset"]*1000),
         }
-      })  
-    })
+      });
+    });
   }
 }
 export default Wetter;
